7fe4b1784a90f5162f70a31441c60d2b73dcbe2d
MarouaneBRomdhane <133340397+MarouaneBRomdhane@users.noreply.github.com>
2024-01-02 10:04:46 +0100

    Delete src directory

--- src/redux/Action.js
+++
@@ -1,8 +0,0 @@
-import { ADDMOVIE, DELETEMOVIE } from "./Actiontype"
-
-export const addmovie=(data)=>{
-    return {type:ADDMOVIE,payload:data}
-}
-export const deletemovie=(id)=>{
-    return {type:DELETEMOVIE,payload:id}
-}
--- src/redux/Actiontype.js
+++
@@ -1,3 +0,0 @@
-export const ADDMOVIE='ADDMOVIE';
-export const DELETEMOVIE='DELETEMOVIE'
-export const EDITMOVIE='EDITMOVIE'
